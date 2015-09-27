96e64cdc7226b971d11c1b6361dffe37662cf258
bianet13 <bia.macedo13@gmail.com>
2015-09-27 15:45:33 -0300

    init commit + improved stopwatch by adding hour/minute/seconds

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    
+    var timer = NSTimer()
+    
+    var time = 0
+    
+    @IBAction func play(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pause(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+    }
+
+    @IBAction func reset(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timerLabel.text = "00:00:00"
+        
+    }
+    
+    let formatter = NSNumberFormatter()
+    
+    func increaseTimer(){
+        
+        time++
+        
+        let (hour, minute, second) = secondsToHoursMinutesSeconds(time)
+        
+        timerLabel.text = "\(formatter.stringFromNumber(hour)!):\(formatter.stringFromNumber(minute)!):\(formatter.stringFromNumber(second)!)"
+        
+    }
+    
+    func secondsToHoursMinutesSeconds(seconds: Int) -> (Int, Int, Int) {
+        return (seconds/3600, (seconds%3600)/60, (seconds%3600)%60)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        formatter.minimumIntegerDigits = 2
     }
 
     override func didReceiveMemoryWarning() {
